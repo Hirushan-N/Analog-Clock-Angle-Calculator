d7bb988502d9a6b9e0a7ea10082f1343621b9590
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-03-20 16:33:14 +0530

    validated user inputs and changed the program.cs as top-level statements

--- Analog Clock Angle Calculator/Program.cs
+++ Analog Clock Angle Calculator/Program.cs
@@ -1,29 +1,22 @@
-﻿using System;
+﻿Console.Write("Enter the hour (1-12): ");
+int hour = int.Parse(Console.ReadLine());
 
-namespace AnalogClockAngleCalculator
+Console.Write("Enter the minute (0-59): ");
+int minute = int.Parse(Console.ReadLine());
+
+if (hour > 0 && minute > 0 && hour <= 12 && minute < 60)
 {
-    class Program
+    double hourAngle = (hour % 12 + minute / 60.0) * 30;
+    double minuteAngle = minute * 6;
+    double angle = Math.Abs(hourAngle - minuteAngle);
+    if (angle > 180)
     {
-        static void Main()
-        {
-            Console.Write("Enter the hour (1-12): ");
-            int hour = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter the minute (0-59): ");
-            int minute = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(hour % 12);
-            Console.WriteLine(minute / 60.0);
-            double hourAngle = (hour % 12 + minute / 60.0) * 30;
-            double minuteAngle = minute * 6;
-            double angle = Math.Abs(hourAngle - minuteAngle);
-            if (angle > 180)
-            {
-                angle = 360 - angle;
-            }
-
-            Console.WriteLine($"The lesser angle between the hour and minute hands is {angle} degrees.");
-        }
+        angle = 360 - angle;
     }
+    Console.WriteLine($"Time is {hour}:{minute} am or pm");
+    Console.WriteLine($"The lesser angle between the hour and minute hands is {angle} degrees.");
 }
-
+else
+{
+    Console.WriteLine($"Invalid Time - {hour}:{minute} am or pm");
+}
