3da83655ee9f9319d63d708213bf81efd20fe207
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-03-20 16:12:10 +0530

    code copleated and tested

--- Analog Clock Angle Calculator/Program.cs
+++ Analog Clock Angle Calculator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AnalogClockAngleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the hour (1-12): ");
+            int hour = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the minute (0-59): ");
+            int minute = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(hour % 12);
+            Console.WriteLine(minute / 60.0);
+            double hourAngle = (hour % 12 + minute / 60.0) * 30;
+            double minuteAngle = minute * 6;
+            double angle = Math.Abs(hourAngle - minuteAngle);
+            if (angle > 180)
+            {
+                angle = 360 - angle;
+            }
+
+            Console.WriteLine($"The lesser angle between the hour and minute hands is {angle} degrees.");
+        }
+    }
+}
+
